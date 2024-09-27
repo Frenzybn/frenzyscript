010a6ece07d86d47688f29310f46a58b14e487e6
Frenzybn <159900158+Frenzybn@users.noreply.github.com>
2024-09-28 01:55:10 +0800

    Update redz_Hub.lua

--- redz_Hub.lua
+++ redz_Hub.lua
@@ -1,4 +1,3 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
 local Window = redzlib:MakeWindow({
   Title = "redz Hub : Blox Fruits",
   SubTitle = "by redz9999",
